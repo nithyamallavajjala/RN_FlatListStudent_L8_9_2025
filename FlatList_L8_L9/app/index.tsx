f9f1f45d33205c0e0188e8553f05eec8baebc4d8
nithyamallavajjala <nithya.mallavajjala@gmail.com>
2025-09-12 08:11:38 -0500

    Update index.tsx

--- FlatList_L8_L9/app/index.tsx
+++ FlatList_L8_L9/app/index.tsx
@@ -1,14 +1,35 @@
-import {
-  Text,
-  View,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
-import colors from "@/styles/colors";
 import defaultStyles from "@/styles/defaultStyles";
+import { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 
 export default function Index() {
+  type dataType = {
+    id: string; //refer to unique identifier
+    title: string; //text we will show in list
+  }
+  //All caps bc data array wont change
+  const DATA: dataType[] = [
+    {id: "1", title: "First Item"},
+    {id: "2", title: "Second Item"},
+    {id: "3", title: "Third Item"},
+    {id: "4", title: "Fourth Item"},
+  ];
+
+  // create a simple function telling me what was selected
+
+  const selectedList = (item: dataType) =>
+  {
+    console.log(item.title);
+    setSelectedId(item.id);
+
+  }
+  const [selectedId, setSelectedId] = useState<string>("1");
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +37,19 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+          data = {DATA}
+          keyExtractor={(item: dataType) => item.id}
+          renderItem={({item}) => (
+            <TouchableOpacity onPress = {() => selectedList(item)}>
+              <View style = {styles.flatListRow}> 
+                <Text>{item.title}</Text>
+              </View>
+            </TouchableOpacity>
+
+          )
+          }
+          />
         </View>
       </View>
     </View>
@@ -26,6 +59,11 @@ export default function Index() {
 const styles = StyleSheet.create({
   flatlist: {
     alignItems: "center",
+    padding: 10,
+  },
+  flatListRow: {
+    backgroundColor: "cadetblue",
+    padding: 10,
   },
   titleContainer: {
     marginTop: 5,
